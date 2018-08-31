52161c85a7e3eb12c36cdea7127f21f0cb00b5b1
Doan Van Quang <quang99dev@gmail.com>
2018-08-31 21:27:09 +0700

    Doctor enxtends

--- src/VSCode/TestJava.java
+++ src/VSCode/TestJava.java
@@ -1,3 +1,29 @@
+public class Doctor {
+    boolean worksAtHospital;
+
+    void treatPatient() {
+        //perfrom a checkup
+    }
+}
+
+public class FamilyDoctor extends Doctor {
+    boolean makesHouseCalls;
+
+    void giveAdvice() {
+        //give homespun advice
+    }
+}
+
+public class Surgeon extends Doctor {
+    void treatPatient() {
+        //Perfrom surgery
+    }
+
+    void makeIncision() {
+        //Make incision (yikes!)
+    }
+}
+
 public class TestJava {
     public static void main(String[] args) {
         String s1 = "Hello.";
